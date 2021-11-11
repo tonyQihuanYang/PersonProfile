fea2d45f525cb1afda2890b9ce5efa0452ae5de3
tony yang <tony@tonydeMBP.hitronhub.home>
2021-11-11 09:35:58 -0600

    commit

--- src/game.ts
+++ src/game.ts
@@ -2,9 +2,35 @@ import 'phaser';
 import * as map from './map/map';
 import Player from './entities/Player';
 
+enum GAME_SETTINGS {
+  Width = 800,
+  Height = 600,
+}
+
+enum MSG_BOX {
+  Y = 400,
+  Padding = 20,
+  Boarder = 5,
+  Radius = 10,
+  Width = GAME_SETTINGS.Width - 2 * MSG_BOX.Padding,
+  Height = GAME_SETTINGS.Height / 3 - MSG_BOX.Padding,
+}
+
+const PLAYER_INITIAL_POSITION = {
+  x: 250,
+  y: 250,
+};
+
 export default class Game extends Phaser.Scene {
   player: Phaser.Physics.Arcade.Sprite;
   cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
+
+  graphics: Phaser.GameObjects.Graphics;
+  text: Phaser.GameObjects.Text;
+  isOverlap: boolean = false;
+
+  circle: any;
+
   constructor() {
     super('Game');
   }
@@ -19,26 +45,113 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
-    const _map = map.create(this);
+    const mapObjects = map.create(this);
+    const _map = mapObjects.map;
+    this.circle = mapObjects.circle;
     this.cursorKeys = this.input.keyboard.createCursorKeys();
 
-    this.player = new Player(this, 25, 25);
+    this.player = new Player(
+      this,
+      PLAYER_INITIAL_POSITION.x,
+      PLAYER_INITIAL_POSITION.y
+    );
     this.cameras.main.startFollow(this.player);
 
+    // Prevent player across the collision, ex trees;
     this.physics.add.collider(
       this.player,
       _map.getLayer(map.MapLayer.Map).tilemapLayer
     );
+
+    // this.physics.add.collider(
+    //   this.player,
+    //   _map.getLayer(map.MapLayer.Map).tilemapLayer
+    // );
+
+    // Do callback if player hit the circle
+    this.initMessageBox();
+    this.addProfileOverlayHandler();
+
+    // var boundsA = this.player.getBounds();
+    //     var boundsB = _circle.getChildren()[0].body.gameObject;
   }
 
-  update(time, delta) {}
+  private addProfileOverlayHandler(): void {
+    this.physics.add.overlap(this.player, this.circle, () =>
+      this.displayProfileMessage()
+    );
+  }
+
+  private displayProfileMessage(): void {
+    if (!this.isOverlap) {
+      this.graphics.x = this.cameras.main.worldView.x;
+      this.graphics.y = this.cameras.main.worldView.y;
+      console.log(this.cameras.main.worldView.x + 2 * MSG_BOX.Padding);
+
+      this.text.x =
+        this.cameras.main.worldView.x + MSG_BOX.Padding + MSG_BOX.Boarder;
+      this.text.y = this.cameras.main.worldView.y + MSG_BOX.Y + MSG_BOX.Boarder;
+      // console.log('overlapping...');
+      this.graphics.setVisible(true);
+      this.text.setVisible(true);
+      this.isOverlap = true;
+    }
+  }
+
+  initMessageBox(): void {
+    this.graphics = this.add.graphics();
+
+    this.graphics.fillStyle(0x786c84, 1);
+    // Grey boarder
+    this.graphics.fillRoundedRect(
+      MSG_BOX.Padding - MSG_BOX.Boarder,
+      MSG_BOX.Y - MSG_BOX.Boarder,
+      MSG_BOX.Width + 2 * MSG_BOX.Boarder,
+      MSG_BOX.Height + 2 * MSG_BOX.Boarder,
+      MSG_BOX.Radius
+    );
+    this.graphics.fillStyle(0xfff9fd, 1);
+    this.graphics.fillRoundedRect(
+      MSG_BOX.Padding,
+      MSG_BOX.Y,
+      MSG_BOX.Width,
+      MSG_BOX.Height,
+      MSG_BOX.Radius
+    );
+    this.text = this.add
+      .text(0, MSG_BOX.Y, 'Fleet Profit Center\nDIU mie', {
+        color: '#000000',
+        align: 'center',
+        wordWrap: { width: MSG_BOX.Width },
+      })
+      .setFontSize(25);
+
+    this.graphics.setVisible(false);
+    this.text.setVisible(false);
+  }
+
+  update(time, delta) {
+    var boundsA = this.player.getBounds();
+    var boundsB = this.circle.getChildren()[0].body;
+    const overlap = Phaser.Geom.Intersects.RectangleToRectangle(
+      boundsA,
+      boundsB
+    );
+    if (this.isOverlap !== overlap && this.isOverlap === true) {
+      this.graphics.setVisible(false);
+      this.text.setVisible(false);
+      this.isOverlap = false;
+    }
+    // console.log(overlap);
+    // console.log(this.isOverlap);
+  }
 }
 
 const config = {
   type: Phaser.AUTO,
   backgroundColor: '#125555',
-  width: 800,
-  height: 600,
+  width: GAME_SETTINGS.Width,
+  height: GAME_SETTINGS.Height,
   scene: Game,
   physics: {
     default: 'arcade',
--- src/map/map.ts
+++ src/map/map.ts
@@ -4,10 +4,20 @@ enum MapLayer {
   Map = 'Ground',
   InformationCircleObject = 'InformationCircle',
   InformationCircleObjectGid = 3,
+
+  UofMTitleSetID = 'UofM',
 }
 
+const firstGid = {
+  Tree: 1,
+  UofM: 181,
+};
+
 const collisionsMap = {
-  Trees: [1, 21],
+  Trees: [0, 20].map((e) => e + firstGid.Tree),
+  UofM: [2, 3, 7, 8, 9, 10, 12, 17, 18, 23, 24, 29, 30, 31, 32, 33, 34, 35].map(
+    (e) => e + firstGid.UofM
+  ),
 };
 
 const loadImage = (scene: Phaser.Scene) => {
@@ -16,12 +26,25 @@ const loadImage = (scene: Phaser.Scene) => {
     frameWidth: 32,
     frameHeight: 32,
   });
+  scene.load.spritesheet('UofM', 'assets/tilesets/UofM.png', {
+    frameWidth: 32,
+    frameHeight: 32,
+  });
 };
 
-const create = (scene: Phaser.Scene): Phaser.Tilemaps.Tilemap => {
-  const tileMap = scene.make.tilemap({ key: 'world' });
-  const mapTiles = tileMap.addTilesetImage('spritesheet', 'spritesheet');
-  tileMap.createLayer(MapLayer.Map, mapTiles);
+const create = (
+  scene: Phaser.Scene
+): { map: Phaser.Tilemaps.Tilemap; circle: Phaser.Physics.Arcade.Group } => {
+  const tileMap = scene.make.tilemap({
+    key: 'world'
+  });
+
+  const uOfM_TitleSet = tileMap.addTilesetImage(
+    MapLayer.UofMTitleSetID,
+    MapLayer.UofMTitleSetID
+  );
+  const treeTileSet = tileMap.addTilesetImage('spritesheet', 'spritesheet');
+  tileMap.createLayer(MapLayer.Map, [treeTileSet, uOfM_TitleSet]);
 
   scene.physics.world.setBoundsCollision(true, true, true, true);
 
@@ -34,8 +57,12 @@ const create = (scene: Phaser.Scene): Phaser.Tilemaps.Tilemap => {
     .getObjectLayer(MapLayer.InformationCircleObject)
     .objects.forEach((element) => {
       if (element.gid === MapLayer.InformationCircleObjectGid) {
-        console.log('Getting here');
-        const circle = infoCircle.create(element.x, element.y, 'spritesheet', element.gid - 1);
+        const circle = infoCircle.create(
+          element.x,
+          element.y,
+          'spritesheet',
+          element.gid - 1
+        );
         circle.setOrigin(0, 1);
       }
     });
@@ -47,9 +74,9 @@ const create = (scene: Phaser.Scene): Phaser.Tilemaps.Tilemap => {
     tileMap.heightInPixels
   );
 
-  tileMap.setCollision(collisionsMap.Trees, true);
+  tileMap.setCollision([...collisionsMap.Trees, ...collisionsMap.UofM], true);
 
-  return tileMap;
+  return { map: tileMap, circle: infoCircle };
 };
 
 export { loadImage, create, MapLayer };
